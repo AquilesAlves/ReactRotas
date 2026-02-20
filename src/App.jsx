d8b3ef295e53705a543b2e92332f5f5fc4ad43e2
Aquiles Alves <aquilespereira789@gmail.com>
2026-02-20 18:41:39 -0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Home from "./pages/Home"
 import Sobre from "./pages/Sobre"
 import Contato from "./pages/Contato"
@@ -5,7 +6,13 @@ import Contato from "./pages/Contato"
 function App() {
   return (
     <div>
-      <Home />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home/>} />
+          <Route path="/sobre" element={<Sobre/>} />
+          <Route path="/contato" element={<Contato/>} />
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
---
+++ src/pages/Menu.jsx
@@ -0,0 +1,11 @@
+function Menu() {
+    return (
+        <div>
+            <a href="/">Home</a>
+            <a href="/sobre">sobre</a>
+            <a href="/contato">contato</a><br />
+        </div>
+    )
+}
+
+export default Menu
